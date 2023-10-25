bcddb727d5af9a7c16bf845a8c94020b1d29a2b5
OgliariNatan <natanogliari@gmail.com>
2023-10-25 12:04:43 -0300

    add comentario em scripty .sql

--- cod/inserir.sql
+++ cod/inserir.sql
@@ -1,1 +1,7 @@
-SELECT * FROM Cliente;
+SELECT * FROM Cliente; --scripty
+SELECT categoria.nome as
+"Tipo", produto.nome as
+"Produto", produto.valor FROM
+Categoria INNER JOIN Produto ON Categoria.Id =
+Produto.Id_Categoria WHERE
+produto.valor< 50.00;
