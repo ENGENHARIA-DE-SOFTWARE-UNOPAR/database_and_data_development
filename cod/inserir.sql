dec0d5aba794d4eb0e8d209a9524b106a9228bdc
OgliariNatan <natanogliari@gmail.com>
2023-10-26 11:46:39 -0300

    update relatório, exemplo inserir.sql

--- cod/inserir.sql
+++ cod/inserir.sql
@@ -5,3 +5,14 @@ SELECT categoria.nome as
 Categoria INNER JOIN Produto ON Categoria.Id =
 Produto.Id_Categoria WHERE
 produto.valor< 50.00;
+
+SELECT
+    escuna.nome AS "escuna",
+    destino.nome AS "ilha",
+    hora_aida, Hora_chegada, date
+FROM passeio
+INNER JOIN escuna
+  ON passeio.escuna_numero = escuna.numero
+INNER JOIN destino
+  on  passeio.destino_id = destino.id
+ORDER BY passeio.date;
