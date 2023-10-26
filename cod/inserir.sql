6e650256a2420f7ba7a134dded1c8ec9ce2a511d
OgliariNatan <natanogliari@gmail.com>
2023-10-26 12:25:24 -0300

    update relatório, exemplo inserir.sql

--- cod/inserir.sql
+++ cod/inserir.sql
@@ -16,3 +16,24 @@ INNER JOIN escuna
 INNER JOIN destino
   on  passeio.destino_id = destino.id
 ORDER BY passeio.date;
+
+-- criação da view ------ tabela temporária
+CREATE VIEW v_consulta AS
+    SELECT
+        escuna.nome AS "escuna",
+        destino.nome AS "ilha",
+        hora_aida, Hora_chegada, date
+    FROM passeio
+    INNER JOIN escuna
+      ON passeio.escuna_numero = escuna.numero
+    INNER JOIN destino
+      on  passeio.destino_id = destino.id
+    ORDER BY passeio.date;
+
+--consulta todas as tabelas
+SHOW tables;
+
+select * from v_consulta;
+
+--após apagar as visões
+DROP view v_consulta;
---
+++ main.lol
@@ -0,0 +1,1 @@
+\contentsline {lstlisting}{\numberline {1}{\ignorespaces inserir.sql}}{6}{lstlisting.1}%
